fdd390c4ea68fd4be5c4a64ccf0a7fde4f631bf6
Ender <neverwas@protonmail.com>
2018-02-04 15:57:32 -0800

    Stuff

--- Voting/client/main.go
+++ Voting/client/main.go
@@ -1,48 +1,74 @@
 package main
 
 import (
-	"crypt/ecdsa",
-	"fmt",
-	"log",
+	//"math/big"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+	"os"
+	//"io/ioutil"
+	"log"
 	"strings"
+	//"encoding/base64"
 )
 
-func RunCmd(cmd string) {
-	if strings.HasPrefix(cmd, "loadfile ") {
-		
-	} else if strings.HasPrefix(cmd, "send ") {
-		SendTo(cmd[4:])
+var key *ecdsa.PrivateKey
+
+/*
+Dear Rob Pike,
+	Go is a decent language.  However, for the sake of all the 'gophers' out there, ADD SOME NORMAL ERROR HANDELING.
+
+Pls don't kill me,
+	The_Sushi/Ender
+*/
+func Handle(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
+}
+
+func RunCmd(cmd string) {
+	if strings.HasPrefix(cmd, "send ") {
+		//Send(strings.Split(cmd, " ")[1], strings.Split(cmd, " ")[2])
+	}
+	
 	switch cmd {
 	case "help":
-		fmt.Println("Command list: help, loadfile {file}, send {ID}, status, genkey, pubkey, privkey")
+		fmt.Println("Command list: help, send {ID}, genkey, pubkey, privkey, exit")
+		
 	case "genkey":
-		GenKey()
-	case "status":
-		fmt.Println( GetInfo() )
+		var err error
+		key, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		Handle(err)
+		
 	case "pubkey":
-		fmt.Println( SmallKey( key.PublicKey().X ) + "::" + SmallKey( key.PublicKey().Y ) )
+		fmt.Println( key.X.String() + "::" + key.Y.String() )
+		
 	case "privkey":
-		fmt.Println( SmallKey(key.D) )
+		fmt.Println( key.D.String() )
+	case "exit":
+		os.Exit(0)
+		
 	default:
 		fmt.Println("Error: Invalid command")
+		
 	}
 }
 
 func main() {
-	fmt.Println(`
+	fmt.Printf("\033[1;35m%s\033[0m\n", `
 		▒█▀▀█ █░░ █▀▀█ █▀▀ █░█ ▒█▀▀█ █▀▀█ ░▀░ █▀▀▄ 
 		▒█▀▀▄ █░░ █░░█ █░░ █▀▄ ▒█░░░ █░░█ ▀█  █░░█ 
 		▒█▄▄█ ▀▀▀ ▀▀▀▀ ▀▀▀ ▀░▀ ▒█▄▄█ ▀▀▀▀ ▀▀▀ ▀░░▀
 	`)
 	fmt.Println(`Enter a command.  Run 'help' if you aren't familiar with the BlockCoin wallet software.`)
 	var input string
-	for 1 {
+	for {
 		fmt.Print("> ")
-		_, err = fmt.Scanln(&input)
-		if err != nil {
-			log.Fatal(err)
-		}
+		_, err := fmt.Scanln(&input)
+		Handle(err)
+		
 		RunCmd(input)
 	}
 }
