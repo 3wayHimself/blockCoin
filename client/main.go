910ae794e602380c6812cffcde496fe523c06204
Ender <neverwas@protonmail.com>
2018-02-04 18:28:25 -0800

    Adding networking stuffs

--- client/main.go
+++ client/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	//"math/big"
+	"crypto"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
 	"os"
-	//"io/ioutil"
+	"net"
 	"log"
 	"strings"
-	//"encoding/base64"
+	//"strconv"
 )
 
 var key *ecdsa.PrivateKey
@@ -26,6 +27,38 @@ func Handle(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Send(from, to, node, amount string, priv *ecdsa.PrivateKey) {
+	var input string
+	conn, err := net.Dial("tcp", node)
+	Handle(err)
+
+	fmt.Println("Connected to node")
+
+	for {
+		fmt.Print("Would you like to send " + amount + "ƁƇ to "+to + " (Y/N): ")
+		_, err := fmt.Scanln(&input)
+		Handle(err)
+		
+		if input == "n" || input == "N" || input == "no" || input == "No" {
+	 		fmt.Println("Cancelled transaction")
+	 		return
+		}	else if input != "Y" && input != "y" && input != "yes" && input != "Yes" {
+			fmt.Println("Not a valid option")
+		} else {
+			break
+		}
+	}
+
+	fmt.Println("Signing...")
+	hashed := sha256.New()
+	hashed.Write([]byte(amount + "->" + to))
+	signed, err := priv.Sign( rand.Reader, hashed.Sum(nil), crypto.SHA256 )
+	Handle(err)
+	fmt.Println("Signed: " + string(signed))
+	fmt.Fprintf(conn, "[%s]:%s->[%s]  %s", from, amount, to, signed)
+	fmt.Println("Sent!")
 }
 
 func RunCmd(cmd string) {
